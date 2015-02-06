3022e51c80852d79cd430465a151789aa6f133a9
Ashish Datta <ashish@setfive.com>
2015-02-06 18:13:19 -0500

    - initial commit!!!

---
+++ web/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app["debug"] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html.twig');
+})->bind("homepage");
+
+$app->get('/submit', function () use ($app) {
+    return $app['twig']->render('done.html.twig');
+});
+
+$app->post('/submit', function () use ($app) {
+    return $app->redirect('/submit');
+})->bind("submit_postcard");
+
+$app->run();
