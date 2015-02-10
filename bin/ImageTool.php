f7d0b54da4f75924bdbd9ab225e7937f991a9c71
Cupidr Live <contact@setfive.com>
2015-02-09 23:13:11 -0500

    - wired everything up!

---
+++ bin/ImageTool.php
@@ -0,0 +1,156 @@
+<?php
+
+error_reporting(E_ALL);
+
+class ImageTool {
+  
+  private $jsonConfigs = [];
+  private $API_KEY;
+  public static $ADDRESS_KEYS = ["name", "address_line1", "address_line2", 
+                                 "address_city", "address_state", "address_zip"];
+  
+  public function __construct($apiKey){
+    $this->API_KEY = $apiKey;
+    $this->jsonConfigs = json_decode(file_get_contents(dirname(__FILE__) . "/templates.json"), true);  
+  }
+
+  public function getTextColor( $im, $template ){
+    $default = imagecolorallocate($im, 0, 0, 0);
+    $config = $this->getConfig( $template );
+    
+    if( !$config || !array_key_exists("color", $config) ){
+      return $default;
+    }
+    
+    $color = str_replace("#", "", $config["color"]);        
+    
+    $r = hexdec($color[0] . $color[1]);
+    $g = hexdec($color[2] . $color[3]);
+    $b = hexdec($color[4] . $color[5]);
+    
+    return imagecolorallocate($im, $r, $g, $b);
+  }  
+  
+  public function getPasteCoords($template){
+    $default = [80, 80];
+    $config = $this->getConfig( $template );
+    
+    if( !$config ){
+      return $default;
+    }
+    
+    if( array_key_exists("left", $config) ){
+      $default[0] += (str_replace("px", "", $config["left"]) * 2.7) - 100;
+      $default[0] = $default[0] < 0 ? 40 : $default[0];
+    }
+        
+    if( array_key_exists("top", $config) ){
+      $default[1] += (str_replace("px", "", $config["top"]) * 2.7) - 100;
+      $default[1] = $default[1] < 0 ? 40 : $default[1];
+    }    
+    
+    return $default;
+  }  
+  
+  public function getNumchars($template){
+    $default = 45;    
+    $config = $this->getConfig( $template );
+    
+    if( !$config || !array_key_exists("max-width", $config) ){
+      return $default;
+    }
+    
+    return ceil( str_replace("px", "", $config["max-width"]) * .06 );
+  }
+  
+  public function getConfig($template){
+        
+    foreach( $this->jsonConfigs as $cfg ){
+      if( $cfg["template"] == $template ){
+        return $cfg["styles"];
+      }
+    }    
+    
+    return null;
+  }
+  
+  
+  public function render($params = [], $filename = null){
+    
+    $template = $params["selected-template"];
+    
+    $numChars = $this->getNumchars($template);    
+    $lines = str_split($params["added-text"], $numChars);
+        
+    $font = dirname(__FILE__) . "/OpenSans-ExtraBold.ttf";
+    $templateImg = imagecreatefromjpeg( dirname(__FILE__) . "/../web/templates/" . $template );
+    
+    $im = imagecreatetruecolor(1875, 1275);
+    
+    $textColor = $this->getTextColor( $im, $template );    
+    $transparentWhite = imagecolorallocatealpha($im, 0, 0, 0, 90);
+    
+    imagesavealpha($im, true);
+    imagefill($im, 0, 0, $transparentWhite);
+    
+    $widths = [];
+    $height = 0;
+    
+    foreach( $lines as $text ){
+      $coords = imagettftext($im, 60, 0, 50, $height + 100, $textColor, $font, $text);
+    
+      $height = $coords[3]; 
+      $widths[] = $coords[2] + 60;
+    }
+    
+    $height += 60;
+    $width = max($widths);
+    
+    $pasteCoords = $this->getPasteCoords($template);           
+    
+    imagecopy($templateImg, $im, $pasteCoords[0], $pasteCoords[1], 0, 0, $width, $height);    
+    
+    if( $filename === null ){
+      header('Content-Type: image/png');
+      imagepng($templateImg);
+    }else{
+      imagepng($templateImg, $filename);
+      chmod($filename, 0755);
+    }
+        
+    imagedestroy($templateImg);    
+  }
+  
+  
+  public function sendPostcard( $to, $from, $urls ){
+    
+    $params = array_merge( ["template" => 1, "full_bleed" => 1], $urls );
+    $addressKeys = ["to" => $to, "from" => $from];
+    
+    foreach( $addressKeys as $addrKey => $addrVars ){
+          
+      foreach( self::$ADDRESS_KEYS as $key ){      
+        if( array_key_exists($key, $addrVars) ){
+          $params[ $addrKey . "[" . $key . "]" ] = $addrVars[$key];
+        }              
+      }
+      
+    }        
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://api.lob.com/v1/postcards/");
+    curl_setopt($ch, CURLOPT_USERPWD, $this->API_KEY);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    
+    $output = curl_exec($ch);
+    $result = json_decode( $output, true );
+    
+    curl_close($ch);
+    
+    return $result;
+  }    
+  
+}
--- web/index.php
+++ web/index.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../config.php';
 
 $app = new Silex\Application();
 
@@ -13,6 +12,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\SwiftmailerServiceProvider());
 
 $app->get('/', function () use ($app) {
 
@@ -29,23 +29,100 @@ $app->get('/', function () use ($app) {
 
     $chunkedArrays = array_chunk($images, 2);
     $templateJson = file_get_contents( dirname(__FILE__) . "/../bin/templates.json" );
-    
+        
     return $app['twig']->render('index.html.twig', ["images" => $chunkedArrays, "templateJson" => $templateJson]);
 })->bind("homepage");
 
-$app->get('/submit', function () use ($app) {
-    return $app['twig']->render('done.html.twig');
-});
+$app->get('/submitted/{fname}', function ($fname) use ($app) {
+    $url = $app['url_generator']->generate("view", ["fname" => $fname]);
+    return $app['twig']->render('done.html.twig', ["url" => $url]);
+})->bind("submitted");
+
+$app->get('/view/{fname}', function ($fname) use ($app) {        
+    return $app['twig']->render('view.html.twig', ["image" => $fname]);
+})->bind("view");
+
+$app->get('/approve/{fname}', function ($fname) use ($app) {
+
+    $pdo = new \PDO(Config::$PDO_CONFIG["dsn"], Config::$PDO_CONFIG["username"], Config::$PDO_CONFIG["password"]);
+    
+    $stmt = $pdo->prepare("SELECT * FROM card WHERE filename = :filename");
+    $stmt->execute(["filename" => $fname]);
+    
+    $rows = $stmt->fetchAll();
+
+    if(count($rows) == 0){
+      die("Card not found?");
+    }
+    
+    $urls = ["front" => "http://cupidr.setfive.com/rendered/" . $fname, 
+             "back" => "http://cupidr.setfive.com/templates/postcard_back.png"];
+    
+    $formData = json_decode($rows[0]["form_fields"], true);
+        
+    $rm = new ImageTool(Config::$LOB_TEST_KEY);
+    $result = $rm->sendPostcard($formData["address"]["to"], $formData["address"]["from"], $urls);  
+   
+    $stmt = $pdo->prepare("UPDATE card SET lob_result = :result, is_sent = true WHERE filename = :filename");
+    $stmt->execute(["filename" => $fname, "result" => json_encode($result)]);   
+   
+    return $app['twig']->render("approve.html.twig", ["result" => print_r($result, true)]);
+})->bind("approve");
 
 $app->post('/submit', function () use ($app) {
 
     $data = $app['request']->request->all();
+    
+    $fname = sha1(uniqid("", true) . rand(0, 100)) . ".png";
+    $filename = dirname(__FILE__) . "/rendered/" . $fname;
+    
+    $rm = new ImageTool();
+    $rm->render( ["added-text" => $data["added-text"], "selected-template" => $data["selected-template"]], $filename );    
+    
     $pdo = new \PDO(Config::$PDO_CONFIG["dsn"], Config::$PDO_CONFIG["username"], Config::$PDO_CONFIG["password"]);
     
-    $stmt = $pdo->prepare("INSERT INTO card (email, form_fields, is_sent) VALUES (:email, :fields, false)");
-    $stmt->execute(["email" => $data["email"], "fields" => json_encode($data)]);
+    $stmt = $pdo->prepare("INSERT INTO card (email, form_fields, filename, is_sent) VALUES (:email, :fields, :filename, false)");
+    $stmt->execute(["email" => $data["email"], "filename" => $fname, "fields" => json_encode($data)]);
     
-    return $app->redirect('/submit');
+    $viewUrl = $app['url_generator']->generate("view", ["fname" => $fname], true);
+    $approveUrl = $app['url_generator']->generate("approve", ["fname" => $fname], true);
+    
+$email = "Hi there-
+
+We've queued your card! We'll review it and send it within the next few hours.
+
+Want to relive your masterpiece? You can view it online at $viewUrl
+
+xoxo
+
+-The cupidr team";
+    
+    $userMessage = \Swift_Message::newInstance()
+                ->setSubject('Cupidr: Your Valentines day postcard')
+                ->setFrom( ['cupidr@setfive.com' => 'The cupidr team'] )
+                ->setTo( [ $data["email"] ] )
+                ->setBody( $email );                           
+    
+    $email = "Hey team,
+    
+View URL: $viewUrl
+Approve URL: $approveUrl
+Form:\n" . print_r($data, true);
+
+    $message = \Swift_Message::newInstance()
+                ->setSubject('Cupidr: Approve card')
+                ->setFrom( ['cupidr@setfive.com' => 'The cupidr team'] )
+                ->setTo( [ "contact@setfive.com" ] )
+                ->setBody( $email );
+
+    if(0){
+      $app['mailer']->send($userMessage);
+      $app['mailer']->send($message);    
+    }
+    
+    $url = $app['url_generator']->generate("submitted", ["fname" => $fname]);    
+    return $app->redirect($url);
+    
 })->bind("submit_postcard");
 
 $app->run();
